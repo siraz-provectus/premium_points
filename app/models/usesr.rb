ef64f6880234cbdf02a7cb233e81e29d3cb51eaa
Airat Sirazutdinov <siraz@yandex.ru>
2019-08-13 17:57:43 +0300

    User model added

---
+++ app/models/usesr.rb
@@ -0,0 +1,2 @@
+class Usesr < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    api_version(module: "v1", path: {value: "v1"}) do
+      resource :transactions, only: [:index, :show, :create]
+    end
+  end
 end
